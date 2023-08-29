96113f0ede49dfe3386a794167d4e50debc1102c
ReoYabiku <alcremikyu.z@gmail.com>
2023-08-29 16:19:09 +0900

    strcpy

--- common.c
+++ common.c
@@ -21,8 +21,19 @@ char *strcpy(char *dst, const char *src) {
     char *d = dst;
     while (*src)
         *d++ = *src++;
-    d* = '\0';
+    *d = '\0';
     return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2) {
+            break;
+        }
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
 }
 
 void printf(const char *fmt, ...) {
